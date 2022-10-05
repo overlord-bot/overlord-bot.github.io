86833b95201be7ae5493fa10433a8e799a1a663a
VNA818-RPI <Arcerv@rpi.edu>
2022-10-05 00:06:11 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,5 +4,8 @@ window.onload = () => {
     //Code here executes when the page is ready
 
     document.body.innerHTML += example();
-    //An example on how to import and use other local JS files
+    /*
+        Appends to the text inside the HTML body tag
+        example function is an example on how to import and use other local JS files
+    */
 };
