2f30ed0fe821f3b826124e9dfdba1af49089ac01
VNA818-RPI <Arcerv@rpi.edu>
2022-10-05 00:11:08 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,12 @@ import { example } from "./components/test.js"
 window.onload = () => {
     //Code here executes when the page is ready
 
+    console.log("Running...")
+    //Prints to the console, useful for debugging, press F12 to view
+
     document.body.innerHTML += example();
     /*
-        Appends to the text inside the HTML body tag
-        example function is an example on how to import and use other local JS files
+    Appends to the text inside the HTML body tag
+    example function is an example on how to import and use other local JS files
     */
 };
