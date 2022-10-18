777ef54f84b0abe46fbc772146eb7816b5a5d85e
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2022-10-18 17:19:44 -0400

    added displaying information for selected major

--- scripts/modules/majors.js
+++ scripts/modules/majors.js
@@ -29,10 +29,63 @@ function sortList() {
 function populateMajors() { //Add majors to search list
     for (var key in class_results) {
         if (class_results.hasOwnProperty(key)) {
-            console.log(key);
+          var ul = document.getElementById("majorUL");
+          var li = document.createElement("li");
+          var a = document.createElement("a");
+
+          a.textContent = key
+          a.setAttribute('href', "#")
+          li.appendChild(a);
+          ul.appendChild(li);
         }
     }
 }
 
+function display_major_info(major){
+  var classes = class_results[major]
+  for (var list_num in classes){
+    var major_class = classes[list_num]
+    console.log(major_class)
+
+    var classDiv = document.getElementById("classesDiv")
+    var typeLi = document.createElement("li");
+    var creditsLi = document.createElement("li");
+    var majorLi = document.createElement("li");
+    var courseLi = document.createElement("li");
+    typeLi.appendChild(document.createTextNode("Type: " + major_class["type"]));
+    creditsLi.appendChild(document.createTextNode("Credits: " + major_class["credits"]));
+    majorLi.appendChild(document.createTextNode("Major: " + major_class["major"]));
+    courseLi.appendChild(document.createTextNode("Course ID: " + major_class["id"]));
+
+    
+    
+
+    classDiv.append(document.createTextNode(major_class["name"]))
+    classDiv.append(typeLi)
+    classDiv.append(creditsLi)
+    classDiv.append(majorLi)
+    classDiv.append(courseLi)
+  }
+}
+
+function clear_past_classes(){
+  document.getElementById('classesDiv').innerHTML = '';
+}
+
 populateMajors()
 
+
+document.getElementById("majorUL").addEventListener("click",function(e) {
+  //var major = window.location.hash.substr(1)
+  var major = e.target.textContent  
+  console.log(e.target.textContent)
+
+  document.getElementById("search_input").value = major
+  sortList()
+  clear_past_classes()
+  display_major_info(major)
+})
+
+
+//var type = window.location.hash.substr(1);
+//console.log(type)
