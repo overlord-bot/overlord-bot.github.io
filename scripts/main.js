462f359fae084318c53ceb5bb1a6bb3616e1c914
zhengdell <pcbcdell1a@gmail.com>
2022-10-21 17:30:51 -0400

    Making the Submit Button Reactive
    Co-Authored-By: Viano Arcery <72038310+VNA818-RPI@users.noreply.github.com>

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,49 @@
 import { example } from "./modules/test.js"
-import { getCatalog, getSchedule} from "./modules/fetch.js"
+import { getCatalog, getSchedule,addCourse} from "./modules/fetch.js"
 
+const deleteCourse =  async (schedule) => {
+    
+}
+
+const getSemester = async (schedule) => {
+    await schedule.schedule.semesters.forEach((element) => {
+        element.forEach((course) => {
+            console.log(course)
+            document.getElementById('freshman-fall').innerHTML += '<tr>' + course.id + ' ' + course.major + ' ' + course.name + '</tr>'
+            document.getElementById('deleted').innerHTML += '<option value=' + course.name.replaceAll(" ", "-") + '>' + course.major + ' ' + course.name + '</option>'
+        })
+    });
+}
 window.onload = async () => {
     //Code here executes when the page is ready
+    document.getElementById("submit").onclick = async () => {
+        document.getElementById('freshman-fall').innerHTML = ''
+        const course = document.getElementById("courses").value.replaceAll("-", " ");;
+        const semester = document.getElementById("semester").value;
+        var dict = {
+            "semester" : semester,
+            "courses" : [course]
+        }
+        console.log(dict)
+        const schedule = await addCourse(1,dict)
+        console.log(await schedule)
+        await getSemester(schedule)
+    };
+    
+    document.getElementById("delete").onclick = async () => {
+        document.getElementById('freshman-fall').innerHTML = ''
+        const course = document.getElementById("courses").value.replaceAll("-", " ");;
+        const semester = document.getElementById("semester").value;
+        var dict = {
+            "semester" : semester,
+            "courses" : [course]
+        }
+        console.log(dict)
+        const schedule = await deleteCourse(1,dict)
+        console.log(await schedule)
+        await getSemester(schedule)
+    
+
 
     console.log("Running...")
     //Prints to the console, useful for debugging, press F12 to view
@@ -11,17 +52,12 @@ window.onload = async () => {
     const catalog = await getCatalog();
     console.log(catalog)
     await catalog.response.courses.forEach((element) => {
-        document.getElementById('courses').innerHTML += '<option value=' + element.name + '>' + element.major + ' ' + element.name + '</option>'
+        document.getElementById('courses').innerHTML += '<option value=' + element.name.replaceAll(" ", "-") + '>' + element.major + ' ' + element.name + '</option>'
     });
 
     const schedule = await getSchedule(1);
     console.log(schedule)
-    await schedule.schedule.semesters.forEach((element) => {
-        element.forEach((course) => {
-            console.log(course)
-            document.getElementById('freshman-fall').innerHTML += '<tr>' + course.id + ' ' + course.major + ' ' + course.name + '</tr>'
-        })
-    });
+    await getSemester(schedule);
     /*
     Appends to the text inside the HTML body tag
     example function is an example on how to import and use other local JS files
--- scripts/modules/fetch.js
+++ scripts/modules/fetch.js
@@ -12,4 +12,32 @@ export const getSchedule = async (num) => {
             .then((data) => res(data));
     })
 }
+export const addCourse = async (num,data) => {
+    return new Promise(async (res, rej) => {
+        fetch('https://vna818.com/api/flask/degree-planner/users/vna/schedule/'+String(num),
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then((response) => response.json())
+            .then((data) => res(data));
+    })
+}
 
+export const deleteCourse = async (num,data) => {
+    return new Promise(async (res, rej) => {
+        fetch('https://vna818.com/api/flask/degree-planner/users/vna/schedule/'+String(num),
+        {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then((response) => response.json())
+            .then((data) => res(data));
+    })
+} 
