e28b849dc28eb806e6cbf6f0719f616f45b080b4
zhengdell <pcbcdell1a@gmail.com>
2022-10-28 17:23:32 -0400

    Submit Delete Button
    Co-Authored-By: Viano Arcery <72038310+VNA818-RPI@users.noreply.github.com>

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,8 @@
 import { example } from "./modules/test.js"
-import { getCatalog, getSchedule,addCourse} from "./modules/fetch.js"
-
-const deleteCourse =  async (schedule) => {
-    
-}
+import { getCatalog, getSchedule,addCourse, deleteCourse} from "./modules/fetch.js"
 
 const getSemester = async (schedule) => {
+    document.getElementById('freshman-fall').innerHTML += '<tr><th>' + 'Freshman Fall' + '</th></tr>'
     await schedule.schedule.semesters.forEach((element) => {
         element.forEach((course) => {
             console.log(course)
@@ -14,50 +11,50 @@ const getSemester = async (schedule) => {
         })
     });
 }
+
 window.onload = async () => {
+    console.log("test")
     //Code here executes when the page is ready
     document.getElementById("submit").onclick = async () => {
-        document.getElementById('freshman-fall').innerHTML = ''
-        const course = document.getElementById("courses").value.replaceAll("-", " ");;
-        const semester = document.getElementById("semester").value;
-        var dict = {
-            "semester" : semester,
-            "courses" : [course]
-        }
-        console.log(dict)
-        const schedule = await addCourse(1,dict)
-        console.log(await schedule)
-        await getSemester(schedule)
+    document.getElementById('freshman-fall').innerHTML = ''
+    const course = document.getElementById("courses").value.replaceAll("-", " ");;
+    const semester = document.getElementById("semester").value;
+    var dict = {
+        "semester" : semester,
+        "courses" : [course]
+    }
+    console.log(dict)
+    const schedule = await addCourse(1,dict)
+    console.log(await schedule)
+    await getSemester(schedule)
     };
-    
     document.getElementById("delete").onclick = async () => {
         document.getElementById('freshman-fall').innerHTML = ''
-        const course = document.getElementById("courses").value.replaceAll("-", " ");;
-        const semester = document.getElementById("semester").value;
+        const course = document.getElementById("deleted").value.replaceAll("-", " ");;
+        //const semester = document.getElementById("").value;
         var dict = {
-            "semester" : semester,
+            "semester" : 0,
             "courses" : [course]
-        }
+            }
         console.log(dict)
         const schedule = await deleteCourse(1,dict)
         console.log(await schedule)
         await getSemester(schedule)
+    };
     
-
-
     console.log("Running...")
     //Prints to the console, useful for debugging, press F12 to view
 
     //document.body.innerHTML += await getCatalog();
     const catalog = await getCatalog();
-    console.log(catalog)
+    console.log("catalog", catalog)
     await catalog.response.courses.forEach((element) => {
         document.getElementById('courses').innerHTML += '<option value=' + element.name.replaceAll(" ", "-") + '>' + element.major + ' ' + element.name + '</option>'
     });
 
-    const schedule = await getSchedule(1);
-    console.log(schedule)
-    await getSemester(schedule);
+    const schedule2 = await getSchedule(1);
+    console.log(schedule2)
+    await getSemester(schedule2);
     /*
     Appends to the text inside the HTML body tag
     example function is an example on how to import and use other local JS files
--- scripts/modules/fetch.js
+++ scripts/modules/fetch.js
@@ -2,14 +2,16 @@ export const getCatalog = async () => {
     return new Promise(async (res, rej) => {
         fetch('https://vna818.com/api/flask/degree-planner/catalog')
             .then((response) => response.json())
-            .then((data) => res(data));
+            .then((data) => res(data))
+            .catch(error => console.log(error));
     })
 }
 export const getSchedule = async (num) => {
     return new Promise(async (res, rej) => {
         fetch('https://vna818.com/api/flask/degree-planner/users/vna/schedule/'+String(num))
             .then((response) => response.json())
-            .then((data) => res(data));
+            .then((data) => res(data))
+            .catch(error => console.log(error));
     })
 }
 export const addCourse = async (num,data) => {
@@ -23,21 +25,24 @@ export const addCourse = async (num,data) => {
             body: JSON.stringify(data),
         })
             .then((response) => response.json())
-            .then((data) => res(data));
+            .then((data) => res(data))
+            .catch(error => console.log(error));
     })
 }
 
 export const deleteCourse = async (num,data) => {
+    console.log(data)
     return new Promise(async (res, rej) => {
         fetch('https://vna818.com/api/flask/degree-planner/users/vna/schedule/'+String(num),
         {
-            method: 'PUT',
+            method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(data),
         })
             .then((response) => response.json())
-            .then((data) => res(data));
+            .then((data) => res(data))
+            .catch(error => console.log(error));
     })
 } 
